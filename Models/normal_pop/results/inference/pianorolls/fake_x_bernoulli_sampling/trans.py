41a678f675289f5214cda6d2bcf93b11e4dbec77
謝宗翰 <han1018>
2021-11-19 16:44:03 +0800

    feat : add Model

--- Models/normal_pop/results/inference/pianorolls/fake_x_bernoulli_sampling/trans.py
+++
@@ -1,4 +0,0 @@
-from pypianoroll import Multitrack
-
-m = Multitrack('./fake_x_bernoulli_sampling_0.npz')
-m.write('./fake_x_bernoulli_sampling_0.mid')
---
+++ exp/normal_origin/results/inference/pianorolls/fake_x_bernoulli_sampling/trans.py
@@ -0,0 +1,4 @@
+from pypianoroll import Multitrack
+
+m = Multitrack('./fake_x_bernoulli_sampling_0.npz')
+m.write('./fake_x_bernoulli_sampling_0.mid')
